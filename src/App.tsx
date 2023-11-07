4950b82f5fa7c6a5c54fe97ebe8238ecc5908cc3
Lachicagladiadora <200379@unsaac.edu.pe>
2023-11-06 22:48:27 -0500

    feat: create repository and template for form

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main>
+      <h1>react hook form</h1>
+      <form action="">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="name" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea placeholder="description" />
+        </div>
+      </form>
+    </main>
+  );
+}
+
+export default App;
